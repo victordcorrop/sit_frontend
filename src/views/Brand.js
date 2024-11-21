38e90fa6be182c11db54ce41df198aed0e4a78f3
miguel-eliezer <migueleliezer1708@gmail.com>
2024-11-21 14:15:07 -0400

    Se hicieron los parametos del registro de marcas

--- src/views/Brand.js
+++ src/views/Brand.js
@@ -1,9 +1,48 @@
+import {
+  Card,
+  Row,
+  Col,
+  CardTitle,
+  CardBody,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
 
-
- const  Brand =() =>{
+const Brand = () => {
   return (
-    <h1>Marcas</h1>
+    <Row>
+      <Col>
+        <Card>
+          <CardTitle tag="h6" className="border-bottom p-3 mb-0">
+            <i className="bi bi-bell me-2"> </i>
+            Registro de Marca
+          </CardTitle>
+          <CardBody>
+            <Form>
+        
+              <FormGroup>
+                <Label for="Marca">Marca</Label>
+                <Input id="Marca" name="Marca" type="text" placeholder="Ingrese la marca que quiera registrar"/>
+              </FormGroup>
+              
+
+              <Button color="info">Submit</Button>
+            </Form>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
   );
-}
+};
+
+
+//  const  Brand =() =>{
+//   return (
+//     <h1>Marcas</h1>
+//   );
+// }
 
 export default Brand
--- src/views/Model.js
+++ src/views/Model.js
@@ -1,9 +1,55 @@
 
 
- const  Model = () => {
+//  const  Model = () => {
+//   return (
+//     <h1>Modelos</h1>
+//   );
+// }
+import {
+  Card,
+  Row,
+  Col,
+  CardTitle,
+  CardBody,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
+
+const Model = () => {
   return (
-    <h1>Modelos</h1>
+    <Row>
+      <Col>
+        <Card>
+          <CardTitle tag="h6" className="border-bottom p-3 mb-0">
+            <i className="bi bi-bell me-2"> </i>
+            Registro de Modelo
+          </CardTitle>
+          <CardBody>
+            <Form>
+            <FormGroup>
+                <Label for="marca">Marca</Label>
+                <Input id="marca" name="marca" type="select">
+                  <option>Marca 1</option>
+                  <option>Marca 2</option>
+                </Input>
+              </FormGroup>
+              
+              <FormGroup>
+                <Label for="modelo">Modelo</Label>
+                <Input id="model" name="model" type="text" placeholder="Ingrese el modelo que quiera registrar"/>
+              </FormGroup>
+              
+
+              <Button color="info">Submit</Button>
+            </Form>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
   );
-}
+};
 
 export default Model;
