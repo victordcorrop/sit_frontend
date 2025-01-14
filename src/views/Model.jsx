b186b14e11435f4c9be97bf1d65dc2fecf3e86f4
desarrollo <desarrollo@desarrollo3.sitssa.gob.ve>
2025-01-14 16:21:45 -0400

    creando funcion pra el select de modelo

--- src/views/Model.jsx
+++ src/views/Model.jsx
@@ -1,10 +1,7 @@
 
 
-//  const  Model = () => {
-//   return (
-//     <h1>Modelos</h1>
-//   );
-// }
+
+import { useEffect } from "react";
 import {
   Card,
   Row,
@@ -19,9 +16,21 @@ import {
 } from "reactstrap";
 
 const Model = () => {
+
+  const selectMarca = async () => {
+    const API_URL = 'http://localhost:3000'
+    const response = await fetch(`${API_URL}/marca`)
+    const marcas = await response.json()
+    return marcas
+
+  }
+
+  const datos = selectMarca()  
+  console.log(datos)
+
   return (
     <Row className="justify-content-md-center">
-      <Col  md={6} >
+      <Col md={6} >
         <Card>
           <CardTitle tag="h6" className="border-bottom p-3 mb-0">
             <i className="bi bi-bell me-2"> </i>
@@ -29,19 +38,18 @@ const Model = () => {
           </CardTitle>
           <CardBody>
             <Form>
-            <FormGroup>
+              <FormGroup>
                 <Label for="marca">Marca</Label>
                 <Input id="marca" name="marca" type="select">
-                  <option>Marca 1</option>
-                  <option>Marca 2</option>
+                  
                 </Input>
               </FormGroup>
 
               <FormGroup>
                 <Label for="modelo">Modelo</Label>
-                <Input id="modelo" name="modelo" type="text" placeholder="Ingrese el modelo que quiera registrar"/>
+                <Input id="modelo" name="modelo" type="text" placeholder="Ingrese el modelo que quiera registrar" />
               </FormGroup>
-              
+
               <div className=" d-flex  justify-content-center">
                 <Button className="mx-8" color="primary" type="submit">Registrar</Button>
               </div>
